209408ea1997541b3bf184c28c1095acd1b35535
Dizzy <itsdizzy40@gmail.com>
2019-01-26 03:34:19 +0100

    Updated .gitignore and added .npmignore

--- lib/interfaces/ICastPerson.d.ts
+++
@@ -1,6 +0,0 @@
-export interface IYtsCastPerson {
-    name: string;
-    character_name: string;
-    url_small_image: string;
-    imdb_code: string;
-}
--- lib/interfaces/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { IYtsMovieData, IYtsMovieListData, IYtsResponse } from "./IResponse";
-export { IYtsMoviesQuery } from "./IMoviesQuery";
-export { IYtsCastPerson } from "./ICastPerson";
-export { IYtsMovieQuery } from "./IMovieQuery";
-export { IYtsTorrent } from "./ITorrent";
-export { IYtsMovie } from "./IMovie";
